34ae4ab8768a60cac74fa85fba45e529c9b481f9
shubhamtunwal <76933039+shubhamtunwal@users.noreply.github.com>
2024-02-06 12:10:29 +0100

    Push check

---
+++ src/main/java/springAnnotations/MainClass.java
@@ -0,0 +1,10 @@
+package springAnnotations;
+
+public class MainClass {
+
+    public static void main( String[] args ){
+        System.out.println("Check");
+    }
+
+
+}
---
+++ src/main/java/springAnnotations/Samsung.java
@@ -0,0 +1,9 @@
+package springAnnotations;
+
+public class Samsung {
+
+    public void config(){
+        System.out.println(" Octa Core, 6 GB Ram");
+    }
+
+}
